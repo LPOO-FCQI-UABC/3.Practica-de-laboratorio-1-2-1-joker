4b12daf2e6834520917b5fc948bb2ef31bb768af
Milkus002 <111380577+Milkus002@users.noreply.github.com>
2022-09-09 14:00:26 -0700

    1

---
+++ Resource.java
@@ -0,0 +1,14 @@
+
+
+public class Resource {
+    private String name;
+
+    public Resource() {
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Task.java
@@ -0,0 +1,29 @@
+public class Task {
+
+    private String name;
+    private int theEmployees;
+    private int theResources;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String x) {
+        this.name = x;
+    }
+
+    public int getTheEmployees() {
+        return theEmployees;
+    }
+    public void setTheEmployees(int y) {
+        this.theEmployees = y;
+    }
+
+    public int getTheResources() {
+        return theResources;
+    }
+    public void setTheResources(int z) {
+        this.theResources = z;
+    }
+
+}
